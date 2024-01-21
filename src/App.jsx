4b25c08d0b10ba0b84852a00b69783baa335e0d6
Ozod <o.bukhorizoda@mail.ru>
2024-01-21 16:40:22 +0500

    created cards components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import { Header } from "./components/header/Header";
-
+import { BigCards } from "./components/bigCards/BigCards";
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <BigCards />
+      </main>
     </>
   );
 }
---
+++ src/components/bigCards/BigCards.jsx
@@ -0,0 +1,56 @@
+import styles from "./BigCards.module.css";
+const bigCardsArray = [
+  {
+    id: 1,
+    socialIcon: "/icon-facebook.svg",
+    username: "@nathanf",
+    followersNum: 1987,
+    text: "Followers",
+    icon: "/icon-up.svg",
+    iconText: "12 Today",
+  },
+  {
+    id: 2,
+    socialIcon: "/icon-twitter.svg",
+    username: "@nathanf",
+    followersNum: 1044,
+    text: "Followers",
+    icon: "/icon-up.svg",
+    iconText: "99 Today",
+  },
+  {
+    id: 3,
+    socialIcon: "/icon-instagram.svg",
+    username: "@realnathanf",
+    followersNum: "11k",
+    text: "Followers",
+    icon: "/icon-up.svg",
+    iconText: "1099 Today",
+  },
+  {
+    id: 4,
+    socialIcon: "/icon-youtube.svg",
+    username: "Nathan F",
+    followersNum: 8239,
+    text: "Subscribers",
+    icon: "/icon-down.svg",
+    iconText: "144 Today",
+  },
+];
+
+export const BigCards = () => {
+  return (
+    <div className={styles.big_cards_container}>
+      {bigCardsArray.map((bigCard) => (
+        <div key={bigCard.id} className={styles.big_card}>
+          <img src={bigCard.socialIcon} alt="" />
+          <p>{bigCard.username}</p>
+          <p>{bigCard.followersNum}</p>
+          <p>{bigCard.text}</p>
+          <img src={bigCard.icon} alt="" />
+          <p>{bigCard.iconText}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
