55352356e4c58494ad61d8e09955f5ce4ce75871
Ozod <o.bukhorizoda@mail.ru>
2024-01-22 18:53:46 +0500

    added a switch

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,17 @@
 import styles from "./Header.module.css";
+import { useState } from "react";
 
 export const Header = () => {
+  const [theme, setTheme] = useState(false);
   return (
-    <header className={styles.header}>
+    <header
+      className={styles.header}
+      style={{
+        backgroundColor: theme ? "black" : "white",
+        transition: "0.3s",
+        color: theme ? "white" : "black",
+      }}
+    >
       <nav>
         <div className={styles.social}>
           <h1>Social Media Dashboard</h1>
@@ -10,6 +19,14 @@ export const Header = () => {
         </div>
         <div className={styles.darkmode}>
           <p>Darkmode</p>
+          <label className={styles.switch}>
+            <input
+              onChange={() => setTheme((prevTheme) => !prevTheme)}
+              type="checkbox"
+              className={styles.inp_checkbox}
+            ></input>
+            <span className={styles.slider}></span>
+          </label>
         </div>
       </nav>
     </header>
